79fdfa166a9f52b0d288dc8e22a431dd47cba60e
maryemmaatallah <maryemmaatallah8@gmail.com>
2026-05-27 02:10:40 +0200

    add sonarcloud configuration

---
+++ sonar-project.properties.php
@@ -0,0 +1,6 @@
+sonar.projectKey=maryemmaatallah_bibliotheque
+sonar.organization=maryemmaatallah
+sonar.projectName=Bibliotheque Laravel Chatbot
+sonar.sources=app,routes,database
+sonar.tests=tests
+sonar.exclusions=vendor/**,node_modules/**,public/**
